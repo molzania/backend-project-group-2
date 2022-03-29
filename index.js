6d54bef982baa12b614688157768b051b107f39f
doloshabell <habelldol@gmail.com>
2022-03-29 12:16:37 +0700

    update config and index

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./config/db");
 const app = express();
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extend: true }));
+
+db
+  .then(() => console.log("Success connect to MongoDB Atlas"))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
   console.log("server online on PORT", PORT);
