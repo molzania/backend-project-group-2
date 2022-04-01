80628e2a2f28aa37804e72dc6c69b9af20358386
mukhid10 <mukhid976@gmail.com>
2022-04-01 10:17:27 +0700

    autenticate

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAll, getByID, addUser } = require("../controllers/user.controller");
+const { getAll, getByID, addUser, addUserLogin } = require("../controllers/user.controller");
 
 router.get("/", getAll);
 router.get("/:id", getByID);
 router.post("/", addUser);
+router.post('/login', addUserLogin)
 
 module.exports = router;
