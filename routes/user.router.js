31ab3f25122e08431fdf91e3205b7dc46067ed10
lucionexus <bossreyshan@gmail.com>
2022-04-01 15:11:54 +0700

    menambah update dan delete pada user

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { getAll, getByID, addUser, addUserLogin } = require("../controllers/user.controller");
+const {
+  getAll,
+  getByID,
+  addUser,
+  addUserLogin,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
 router.get("/", getAll);
 router.get("/:id", getByID);
 router.post("/", addUser);
-router.post('/login', addUserLogin)
+router.post("/login", addUserLogin);
+router.post("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
